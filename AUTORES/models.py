b4fd48baa7fd14c0d7ff23efafb95edd11fac3a3
landersoft <landersoft@gmail.com>
2019-07-30 22:43:09 -0400

    Se agrega el modelo Autor, y se crea la BD

--- AUTORES/models.py
+++ AUTORES/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    rut = models.IntegerField()
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
